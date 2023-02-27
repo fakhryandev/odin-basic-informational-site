470fadcdf54f8c7cf3eb7991c7243a46f2763a83
Muhammad Fakhryan <fakhryandev@gmail.com>
2023-02-27 22:48:28 +0700

    refactor

--- index.js
+++ index.js
@@ -5,29 +5,37 @@ const hostname = "127.0.0.1";
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    fs.readFile("./index.html", (err, data) => {
-      res.writeHeader(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url == "/about") {
-    fs.readFile("./about.html", (err, data) => {
-      res.writeHeader(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url == "/contact-me") {
-    fs.readFile("./contact-me.html", (err, data) => {
-      res.writeHeader(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+  res.writeHeader(200, { "Content-Type": "text/html" });
+
+  let path = "./pages";
+
+  const url = req.url;
+  switch (url) {
+    case "/":
+      path = `${path}/index.html`;
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path = `${path}/about.html`;
+      res.statusCode = 200;
+      break;
+    case "/contact":
+      path = `${path}/contact-me.html`;
+      res.statusCode = 200;
+      break;
+    default:
+      path = `${path}/404.html`;
+      res.statusCode = 404;
+      break;
   }
-  fs.readFile("./404.html", (err, data) => {
-    res.writeHeader(200, { "Content-Type": "text/html" });
-    res.write(data);
-    res.end();
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
   });
 });
 
